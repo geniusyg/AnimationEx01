411f8ec3e035c1a4cdb434361c166018da361625
Chae Yungi <l8673901@gmail.com>
2014-01-08 18:28:10 +0900

    AnimationEx01
    AnimationEx01

--- AnimationEx01/ViewController.m
+++ AnimationEx01/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () {
+	BOOL moveFlag;
+}
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
@@ -25,5 +28,79 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)series:(id)sender {
+	[UIView beginAnimations:@"series1" context:NULL];
+	[UIView setAnimationDelegate:self];
+	[UIView setAnimationDuration:1.0];
+	[UIView setAnimationDelegate:self];
+	[UIView setAnimationDidStopSelector:@selector(changeBackgroundClear)];
+	self.imageView.backgroundColor = [UIColor whiteColor];
+	[UIView commitAnimations];
+	
+//	[UIView animateWithDuration:1.0 animations:^{
+//		self.imageView.backgroundColor = [UIColor whiteColor];
+//	} completion:^(BOOL finished){
+//		[UIView animateWithDuration:1.0 animations:^{
+//			self.imageView.backgroundColor = [UIColor clearColor];
+//		}];
+//	}];
+}
+- (void)changeBackgroundClear {
+	[UIView beginAnimations:@"series2" context:NULL];
+	[UIView setAnimationDuration:1.0];
+	[UIView setAnimationDelegate:self];
+	self.imageView.backgroundColor = [UIColor clearColor];
+	[UIView commitAnimations];
+}
+- (IBAction)move:(id)sender {
+	[UIView beginAnimations:nil context:NULL];
+	[UIView setAnimationDuration:0.7];
+	
+	if(moveFlag)
+		self.imageView.center = CGPointMake(self.imageView.center.x+100, self.imageView.center.y+100);
+	else
+		self.imageView.center = CGPointMake(self.imageView.center.x - 100, self.imageView.center.y - 100);
+	
+	moveFlag = !moveFlag;
+	
+	[UIView commitAnimations];
+}
+- (IBAction)alpha:(id)sender {
+//	NSLog(@"Changing Alpha");
+//	[UIView beginAnimations:nil context:NULL];
+//	[UIView setAnimationDuration:1.0];
+//	[UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
+//	
+//	self.imageView.alpha = (self.imageView.alpha == 0.0) ? 1.0 : 0.0;
+	
+	[UIView animateWithDuration:0.7 animations:^{
+		self.imageView.alpha = (self.imageView.alpha == 0.0) ? 1.0 : 0.0;
+	}];
+}
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
